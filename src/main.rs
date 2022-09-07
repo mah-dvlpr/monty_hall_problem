d6e47d1a83e22f74e7c0f482a7c73cbf74a746d0
mah-dvlpr <mah.dvlpr@gmail.com>
2022-09-07 23:49:38 +0200

    Updated solution without vector for doors. Might be less intuitive, but way faster! Also some formatting improvements (made by rust-analyzer).

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 /// Monty Hall Problem numerical test (not proof)
-
-use rand::{thread_rng, Rng};
 use clap::Parser;
+use rand::{thread_rng, Rng};
 
 #[derive(Parser)]
 struct Args {
-   #[clap(short, long)]
-   iterations: usize,
+    #[clap(short, long)]
+    iterations: usize,
 
-   #[clap(short, long)]
-   num_doors: usize,
-}    
+    #[clap(short, long)]
+    num_doors: usize,
+}
 
 fn main() {
     let args = Args::parse();
@@ -27,31 +26,19 @@ fn main() {
 
     for _ in 0..iterations {
         // Set one door as "correct" (with prize)
-        let mut doors = vec![false; num_doors];
         let correct = rng.gen_range(0..num_doors);
-        doors[correct] = true;
 
         // Make a random guess
         let guess = rng.gen_range(0..num_doors);
 
-        // Gather data for if "simple" tactic is correct.
-        simple.push(doors[guess]);
+        // Gather data for if "simple" tactic is correct (do not switch).
+        simple.push(guess == correct);
 
-        // Gather data for if "smart" tactic is correct.
-        // Truncate all wrong options except guess and correct (in new vector). Would be done by the show host.
-        if guess != correct {
-            doors = vec![doors[guess], doors[correct]];
-        } else {
-            // "Keep a bad door"
-            doors = vec![doors[guess], false];
-        }
-        
-        // From logic above, "switching" means always picking the second door (index 1),
-        // since our initial guess will have index 0.
-        smart.push(doors[1]);
+        // Gather data for if "smart" tactic is correct (always switch).
+        smart.push(guess != correct);
     }
 
-    let print_format = |prefix: &str, v: &Vec::<bool>| {
+    let print_format = |prefix: &str, v: &Vec<bool>| {
         let fun = |acc, &x| {
             if x {
                 acc + 1
@@ -59,7 +46,11 @@ fn main() {
                 acc
             }
         };
-        println!("{}: {:.8}%", prefix, (v.iter().fold(0, fun) as f64 / iterations as f64) * 100_f64);
+        println!(
+            "{}: {:.8}%",
+            prefix,
+            (v.iter().fold(0, fun) as f64 / iterations as f64) * 100_f64
+        );
     };
 
     print_format("Simple", &simple);
